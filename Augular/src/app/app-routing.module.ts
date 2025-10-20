3a3b34055e4a78a7e0ba8d258d975362341d33af
gleb1628 <117732048+gleb1628@users.noreply.github.com>
2025-10-20 23:18:03 +0300

    app-routing.module.ts

--- Augular/src/app/app-routing.module.ts
+++ Augular/src/app/app-routing.module.ts
@@ -5,14 +5,12 @@ import { LairComponent } from './components/lair/lair.component';
 import { AbyssComponent } from './components/abyss/abyss.component';
 
 const routes: Routes = [
-  // Редирект с главной
   { path: '', redirectTo: '/breakfast/all', pathMatch: 'full' },
 
-  // Наши страницы по типам еды
   { 
     path: 'breakfast/:difficulty', 
     component: VaultComponent,
-    data: { mealRite: 'Breakfast' } // Передаем "тип еды" через data
+    data: { mealRite: 'Breakfast' } 
   },
   { 
     path: 'lunch/:difficulty', 
@@ -25,16 +23,16 @@ const routes: Routes = [
     data: { mealRite: 'Dinner' } 
   },
 
-  // Страница конкретного рецепта
   { path: 'recipe/:id', component: LairComponent },
 
-  // Страница 404
+
   { path: '404', component: AbyssComponent },
-  { path: '**', redirectTo: '/404' } // Любой другой путь кидаем на 404
+  { path: '**', redirectTo: '/404' } 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
