d3a31b5dc69b13aafc4c8fa0b55f6af23a6fb91f
gleb1628 <117732048+gleb1628@users.noreply.github.com>
2025-10-20 23:19:02 +0300

    app.module.ts

--- Augular/src/app/app.module.ts
+++ Augular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'; // Важно для API
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,9 +18,10 @@ import { AbyssComponent } from './components/abyss/abyss.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule // Добавляем модуль
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
